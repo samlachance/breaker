c95a4a14aa7a3d4dc8d3a1ebf82cb8cd19cd0870
Sam Lachance <slachance@gmail.com>
2016-02-22 16:33:55 -0500

    Added break countdown

--- breaker.sh
+++ breaker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 while : 
 do
+  
   let "c=$1"
+  let "break=$2"
+
   while [ $c -gt 0 ]; do
     printf "\033c"
     echo "BREAKER"
@@ -9,5 +12,17 @@ do
     let "c=$c-1"
     sleep 60
   done
+
   echo "time to take a break" | festival --tts
+
+  while [ $break -gt 0 ]; do
+    printf "\033c"
+    echo "BREAKER"
+    echo "Break time for the next" $break "minutes."
+    let "break=$break-1"
+    sleep 60
+  done
+
+  echo "break is over" | festival --tts
+
 done
